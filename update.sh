b573fe57583f6e75702d1f60e7aba90824642f91
Jesse Rivera <jssrvr27@gmail.com>
2025-07-20 13:22:44 -0500

    update ollama too

--- update.sh
+++ update.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 
+echo "Starting the update process..."
+set -e
+echo "Updating Ollama ..."
+curl -fsSL https://ollama.com/install.sh | sh
+
+echo "Updating Docker containers ..."
 docker run --rm \
     --volume /var/run/docker.sock:/var/run/docker.sock \
     containrrr/watchtower \
